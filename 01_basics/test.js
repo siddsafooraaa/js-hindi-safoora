861d55d5c25abc93ff2e4bf14ef879fda6dde7e1
siddsafooraaa <siddsafooraaa@gmail.com>
2025-11-25 09:30:37 +0000

    study

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("safoora")
+//console.log("safoora");
+const name=" Safoora@gmail.com "
+/*const last="Siddiqui"
+console.log(`hello my name is ${name} and my surname is ${last}`);
+const game=new String('Safoora')
+console.log(game[3])
+console.log(game.length)
+console.log(game.charAt('5'));
+console.log(game.charAt('o'))
+console.log(name.trim())
+console.log(name)*/
+console.log(name.replace('@','123'))
+console.log(name.includes('123'))
+console.log(name.split('@'))
