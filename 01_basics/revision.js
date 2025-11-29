08f3fc232881d62c806f950bc9e9ae1744641dc5
siddsafooraaa <siddsafooraaa@gmail.com>
2025-11-29 08:27:10 +0000

    huuu

---
+++ 01_basics/revision.js
@@ -0,0 +1,16 @@
+/*let score="33"
+console.log(score)
+let value=Number(score)
+console.log(value)
+console.log(2**4)
+console.log("1"+2+2)
+console.log(1+2+3+"4")
+console.log(true)
+console.log(+true)*/
+/*console.log(null>0)
+console.log(null>=0)*/
+let Myname="Safoora"
+let another=Myname
+another="Siddiqui"
+console.log(Myname)
+console.log(another)
